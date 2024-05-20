dceadb90e76dd6cf8fa434d2b8dc0b82405a3372
james-hilliard-3 <42178698+james-hilliard-3@users.noreply.github.com>
2024-05-19 23:19:44 -0400

    Delete locals.tf

--- locals.tf
+++
@@ -1,9 +0,0 @@
-
-locals {
-  prod_rg_name  = "${var.resource_prefix}-prod-rg"
-  qa_rg_name    = "${var.resource_prefix}-qa-rg"
-  test_rg_name  = "${var.resource_prefix}-test-rg"
-  prod_vnet_name = "${var.resource_prefix}-prod-vnet"
-  qa_vnet_name = "${var.resource_prefix}-qa-vnet"
-  test_vnet_name = "${var.resource_prefix}-test-vnet"
-}
